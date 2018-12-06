a79a8bfb4d16b2c64eb815fb88d20f8ac713e810
Partha Aji <paji@redhat.com>
2018-12-05 23:48:09 -0500

    Added Itunes search app

--- main.js
+++ main.js
@@ -1,33 +1,40 @@
 
-// function search(params) {
-//   var queryString = Object.keys(params).map(key => key + '=' + params[key]).join('&');
-//   fetch('https://itunes.apple.com/search?${queryString}')
-//     .then(function(response) {
-//       return response.json();
-//     })
-//     .then(function(myJson) {
-//       console.log(JSON.stringify(myJson));
-//     });
-// }
-
 class ITunesApp {
   constructor() {
-    this.searchResultsElement = document.getElementById("search-results");
-    this.searchButton = document.getElementById("search-button");
-    this.searchText = document.getElementById("search-bar");
+    this.searchResultsElement = $("#search-results");
+    this.searchButton = $("#search-button");
+    this.searchText = $("#search-bar");
+    this.results = $("#results");
+    this.audio = $("#audio");
+    this.audioPlayer = $("#player");
     this.setupEventListeners();
   }
 
   setupEventListeners() {
-    this.searchButton.addEventListener("click", this.search.bind(this));
+    this.searchButton.click(this.search.bind(this));
+    this.results.click(this.trackClicked.bind(this))
+  }
+
+  trackClicked(event) {
+    this.audio.attr("src", event.target.dataset["url"]);
+    this.audioPlayer[0].pause();
+    this.audioPlayer[0].load();
   }
 
   searchResults(resultsJson) {
-    console.log(JSON.stringify(resultsJson));
+    let results = resultsJson["results"];
+    this.results.children().remove();
+    for(let i=0; i< results.length; i++) {
+      let result = results[i];
+      let html= `<li data-url='${result.previewUrl}'> Track:${result.trackName}, Artist:${result.artistName}, Album: ${result.collectionName} </li>`;
+      this.results.append(html);
+    }
   }
+
   search() {
-    let input = this.searchText.value;
-    fetch('https://itunes.apple.com/search?term=${input}')
+    let input = this.searchText.val();
+    fetch(`https://itunes-api-proxy.glitch.me/search?media=music&term=${input}`, {
+      })
       .then(function(response) {
         return response.json();
       })
